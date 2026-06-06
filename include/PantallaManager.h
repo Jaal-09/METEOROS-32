9d40f75aff38b0a170d734c76ad6fb0664c6a659
Jaal-09 <jaal9709+github@gmail.com>
2026-06-06 15:30:30 -0500

    Se agrego la libreria para implementar el modulo de fecha y hora, tambien se hicieron cambios en la visualizacion en PantallaManager  para mostar 2 vistas en pantalla

--- include/PantallaManager.h
+++ include/PantallaManager.h
@@ -7,13 +7,17 @@
 class PantallaManager {
 private:
     TFT_eSPI tft;
+    // 1 = Principal (Hora/Fecha/Temp), 2 = Secundaria (Hum/Lux)
+    int vistaActual; 
 
 public:
     PantallaManager();
     void iniciar();
     void limpiarPantalla(uint16_t color);
-    void mostrarTexto(String texto, int x, int y, int tamano, uint16_t color);
-    void actualizarInterfaz(float temp, float hum, float lux);
+    void mostrarTexto(String texto, int x, int y, int tamano, uint16_t color, uint16_t fondo);
+    // Al tocar la pantalla cambiará de estado
+    void cambiarVista(); 
+    void actualizarInterfaz(String hora, String fecha, float temp, float hum, float lux);
 };
 
 #endif
--- src/PantallaManager.cpp
+++ src/PantallaManager.cpp
@@ -1,62 +1,67 @@
 #include "PantallaManager.h"
 
-//Los pines se cargan directamente desde el platformio.ini
 PantallaManager::PantallaManager() {
-    
+    // Arranca en la pantalla principal
+    vistaActual = 1; 
 }
 
 void PantallaManager::iniciar() {
-
     tft.init();
-    tft.setRotation(1); 
 
-    tft.fillRect(0, 0, 320, 240, TFT_NAVY); 
-    delay(50);
-
-    //Banner
-    tft.fillRect(0, 0, 320, 38, TFT_BLUE);
-
-    // Configuración del título
-    tft.setTextColor(TFT_WHITE, TFT_BLUE);
-    tft.setTextSize(2);
-    tft.drawString("ESTACION METEOROLOGICA", 30, 10);
-
-    //Contenedores para la información de ejemplo a mostrar
-    tft.drawRect(10, 50, 300, 45, TFT_RED);
-    tft.drawRect(10, 105, 300, 45, TFT_CYAN);
-    tft.drawRect(10, 160, 300, 45, TFT_YELLOW);
-
-    // Pie de página fijo
-    tft.drawLine(0, 218, 320, 218, TFT_SILVER);
-    tft.setTextColor(TFT_SILVER, TFT_NAVY);
-    tft.setTextSize(1);
-    tft.drawString("U. DE PAMPLONA - MECATRÓNICA", 45, 224);
+    // Modo horizontal
+    tft.setRotation(1);       
+    limpiarPantalla(TFT_NAVY); 
 }
 
 void PantallaManager::limpiarPantalla(uint16_t color) {
     tft.fillScreen(color);
 }
 
+void PantallaManager::cambiarVista() {
+    vistaActual = (vistaActual == 1) ? 2 : 1;
+    
+    // Limpieza rápida al cambiar de menú
+    limpiarPantalla(TFT_NAVY); 
+}
 
-void PantallaManager::mostrarTexto(String texto, int x, int y, int tamano, uint16_t color) {
-    //Color para el texto
-    tft.setTextColor(color, TFT_NAVY);
-
-    //tamaño del texto
+void PantallaManager::mostrarTexto(String texto, int x, int y, int tamano, uint16_t color, uint16_t fondo) {
+    tft.setTextColor(color, fondo);
     tft.setTextSize(tamano);
-
-    //dibujamos
     tft.drawString(texto, x, y); 
 }
 
-void PantallaManager::actualizarInterfaz(float temp, float hum, float lux) {
-    // Mandamos los datos con coordenadas ajustadas para que queden centrados en los rectángulos
-    String strTemp = "TEMP: " + String(temp, 1) + " C ";
-    mostrarTexto(strTemp, 25, 62, 3, TFT_RED);
-    
-    String strHum = "HUM:  " + String(hum, 1) + " % ";
-    mostrarTexto(strHum, 25, 117, 3, TFT_CYAN);
-    
-    String strLux = "LUX:  " + String(lux, 0) + " lx ";
-    mostrarTexto(strLux, 25, 172, 3, TFT_YELLOW);
+void PantallaManager::actualizarInterfaz(String hora, String fecha, float temp, float hum, float lux) {
+    // Banner Superior Común para ambas vistas
+    tft.fillRect(0, 0, 320, 38, TFT_BLUE);
+    mostrarTexto("ESTACION METEOROLOGICA", 30, 10, 2, TFT_WHITE, TFT_BLUE);
+
+    if (vistaActual == 1) {
+        // ================= VISTA 1: TIEMPO Y TEMPERATURA =================
+        // Recuadro de Hora y Fecha
+        tft.drawRect(10, 50, 300, 75, TFT_GREEN);
+        mostrarTexto("FECHA: " + fecha, 20, 60, 2, TFT_GREEN, TFT_NAVY);
+        mostrarTexto("HORA:  " + hora, 20, 90, 3, TFT_WHITE, TFT_NAVY);
+
+        // Recuadro de Temperatura justo debajo
+        tft.drawRect(10, 135, 300, 55, TFT_RED);
+        mostrarTexto("TEMP: " + String(temp, 1) + " C", 20, 150, 3, TFT_RED, TFT_NAVY);
+        
+        mostrarTexto("-> Toca para ver mas", 80, 200, 1, TFT_SILVER, TFT_NAVY);
+    } 
+    else {
+        // ================= VISTA 2: DETALLES (HUMEDAD Y LUZ) =================
+        // Recuadro Humedad
+        tft.drawRect(10, 50, 300, 60, TFT_CYAN);
+        mostrarTexto("HUMEDAD: " + String(hum, 1) + " %", 20, 68, 3, TFT_CYAN, TFT_NAVY);
+
+        // Recuadro Iluminación
+        tft.drawRect(10, 120, 300, 60, TFT_YELLOW);
+        mostrarTexto("LUZ: " + String(lux, 0) + " lx", 20, 138, 3, TFT_YELLOW, TFT_NAVY);
+        
+        mostrarTexto("-> Toca para regresar", 80, 200, 1, TFT_SILVER, TFT_NAVY);
+    }
+
+    // Pie de página institucional
+    tft.drawLine(0, 218, 320, 218, TFT_SILVER);
+    mostrarTexto("U. DE PAMPLONA - MECATRONICA", 45, 224, 1, TFT_SILVER, TFT_NAVY);
 }
