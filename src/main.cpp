6f65ffc3a61d24c72e2426ac25d7b14e0eb47222
Jaal-09 <jaal9709+github@gmail.com>
2026-06-05 19:53:10 -0500

    Se implementa un loop para borrar la pantalla al iniciar y luego volver a pintar los datos en sus coordenadas, main terminado para visualizar texto de ejemplo en la pantalla

--- src/main.cpp
+++ src/main.cpp
@@ -19,3 +19,28 @@ void setup() {
     miPantalla.mostrarTexto("Iniciando Panel TFT ...", 60, 100, 2, TFT_WHITE);
     delay(1500);
 }
+
+void loop() {
+    if (millis() - tiempoUltimaActualizacion >= INTERVALO_TFT) {
+        tiempoUltimaActualizacion = millis();
+        
+        //Se borra el texto de iniciando pantalla
+        if (primeraActualizacion) {
+            // Se usa la función limpiarPantalla
+            miPantalla.limpiarPantalla(TFT_NAVY);
+            
+            // Volvemos a llamar a iniciar para que redibuje los marcos fijos y el título
+            miPantalla.iniciar(); 
+            
+            // Apagamos la bandera para que no lo vuelva a hacer
+            primeraActualizacion = false; 
+        }
+        
+        float t_simulada = 26.7;
+        float h_simulada = 55.2;
+        float l_simulada = 385.0;
+        
+        Serial.println("[SISTEMA] Refrescando interfaz grafica.");
+        miPantalla.actualizarInterfaz(t_simulada, h_simulada, l_simulada);
+    }
+}
