68538d4b264c938485604ba91f52357c9a9ec187
Jaal-09 <jaal9709+github@gmail.com>
2026-06-06 18:03:28 -0500

    Empezando la implementacion para el manejo del sensor bme-280

---
+++ src/Bme280Manager.cpp
@@ -0,0 +1,19 @@
+#include "Bme280Manager.h"
+
+Bme280Manager::Bme280Manager() {
+    sensorOperativo = false;
+}
+
+bool Bme280Manager::iniciar() {
+    Serial.println("[BME280] Iniciando bus I2C secundario...");
+    
+    //Pines para el bme-280
+    bool busIniciado = Wire1.begin(18, 19); 
+    
+    if (!busIniciado) {
+        Serial.println("[ERROR] No se pudo inicializar el bus I2C en pines 18 y 19.");
+        return false;
+    }
+
+
+}
