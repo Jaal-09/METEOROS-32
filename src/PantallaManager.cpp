bbaa90765c6cf99c5f261fd5f1f8bf8fc85a0e1e
Jaal-09 <jaal9709+github@gmail.com>
2026-06-05 19:25:27 -0500

    Agregamos 3 contenedores para la información a mostrar y tambien un pie de pagina

--- src/PantallaManager.cpp
+++ src/PantallaManager.cpp
@@ -21,6 +21,14 @@ void PantallaManager::iniciar() {
     tft.setTextSize(2);
     tft.drawString("ESTACION METEOROLOGICA", 30, 10);
 
+    //Contenedores para la información de ejemplo a mostrar
+    tft.drawRect(10, 50, 300, 45, TFT_RED);
+    tft.drawRect(10, 105, 300, 45, TFT_CYAN);
+    tft.drawRect(10, 160, 300, 45, TFT_YELLOW);
 
-
+    // Pie de página fijo
+    tft.drawLine(0, 218, 320, 218, TFT_SILVER);
+    tft.setTextColor(TFT_SILVER, TFT_NAVY);
+    tft.setTextSize(1);
+    tft.drawString("U. DE PAMPLONA - MECATRÓNICA", 45, 224);
 }
