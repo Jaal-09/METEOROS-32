ffdf10b591f6210ebdfa003594f42977898762ab
Jaal-09 <jaal9709+github@gmail.com>
2026-06-05 19:34:59 -0500

    Implementamos las funciones para limpiar la pantalla, mostar el texto y pintar o dibujar el texto en las coordenadas correspondientes y tambien se especifico el driver de la pantalla tft y la resolucion en el archivo de inicio

--- src/PantallaManager.cpp
+++ src/PantallaManager.cpp
@@ -32,3 +32,31 @@ void PantallaManager::iniciar() {
     tft.setTextSize(1);
     tft.drawString("U. DE PAMPLONA - MECATRÓNICA", 45, 224);
 }
+
+void PantallaManager::limpiarPantalla(uint16_t color) {
+    tft.fillScreen(color);
+}
+
+
+void PantallaManager::mostrarTexto(String texto, int x, int y, int tamano, uint16_t color) {
+    //Color para el texto
+    tft.setTextColor(color, TFT_NAVY);
+
+    //tamaño del texto
+    tft.setTextSize(tamano);
+
+    //dibujamos
+    tft.drawString(texto, x, y); 
+}
+
+void PantallaManager::actualizarInterfaz(float temp, float hum, float lux) {
+    // Mandamos los datos con coordenadas ajustadas para que queden centrados en los rectángulos
+    String strTemp = "TEMP: " + String(temp, 1) + " C ";
+    mostrarTexto(strTemp, 25, 62, 3, TFT_RED);
+    
+    String strHum = "HUM:  " + String(hum, 1) + " % ";
+    mostrarTexto(strHum, 25, 117, 3, TFT_CYAN);
+    
+    String strLux = "LUX:  " + String(lux, 0) + " lx ";
+    mostrarTexto(strLux, 25, 172, 3, TFT_YELLOW);
+}
